e0b92f8fb0e0556afe16b4ee22c37184567aa433
g01100001 <gomandam@student.42madrid.com>
2025-08-13 02:55:16 +0200

    [add] recursive executor, functions

--- source/execute/exec.c
+++ source/execute/exec.c
@@ -6,7 +6,7 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 00:24:27 by gomandam          #+#    #+#             */
-/*   Updated: 2025/08/13 02:42:28 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/08/13 02:50:40 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,3 +53,130 @@ struct s_ast {
         t_subsh subsh;
     } u_data;
 };
+
+// Utility for pipes (left | right)
+int exec_pipe_node(t_ast *left, t_ast *right)
+{
+    int fd[2];
+    pid_t pid_left, pid_right;
+    int status = 0;
+
+    if (pipe(fd) == -1)
+        return (perror("pipe"), 1);
+
+    pid_left = fork();
+    if (pid_left == 0)
+    {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]); close(fd[1]);
+        exit(exec_ast(left));
+    }
+
+    pid_right = fork();
+    if (pid_right == 0)
+    {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]); close(fd[1]);
+        exit(exec_ast(right));
+    }
+
+    close(fd[0]); close(fd[1]);
+    waitpid(pid_left, &status, 0);
+    waitpid(pid_right, &status, 0);
+    return status;
+}
+// The recursive executor (Prompt from chatgpt. I have found bigs)
+int exec_ast(t_ast *node)
+{
+    int s;
+    if (!node)
+        return 1;
+    if (node->type == AST_CMD)
+        return exec_cmd_node(node->u_data.cmd.argv);
+
+    if (node->type == AST_PIPE)
+        return exec_pipe_node(node->u_data.op.left, node->u_data.op.right);
+
+    if (node->type == AST_AND_IF)
+    {
+        s = exec_ast(node->u_data.op.left);
+        if (WIFEXITED(s) && WEXITSTATUS(s) == 0)
+            return exec_ast(node->u_data.op.right);
+        return s;
+    }
+    if (node->type == AST_OR_IF)
+    {
+        s = exec_ast(node->u_data.op.left);
+        if (WIFEXITED(s) && WEXITSTATUS(s) != 0)
+            return exec_ast(node->u_data.op.right);
+        return s;
+    }
+    if (node->type == AST_SUBSH)
+    {
+        pid_t pid = fork();
+        int status = 0;
+        if (pid == 0)
+            exit(exec_ast(node->u_data.subsh.child));
+        else if (pid > 0)
+            waitpid(pid, &status, 0);
+        else
+            perror("fork");
+        return status;
+    }
+    return 1;
+}
+// Debug: build a small AST for testing
+t_ast *build_test_ast(void)
+{
+    // Prepare command: /bin/echo hello
+    t_ast *echo_node = malloc(sizeof(t_ast));
+    echo_node->type = AST_CMD;
+    char **argv = malloc(sizeof(char*) * 3);
+    argv[0] = "/bin/echo";
+    argv[1] = "hello";
+    argv[2] = NULL;
+    echo_node->u_data.cmd.argv = argv;
+
+    // Prepare command: /bin/ls -l
+    t_ast *ls_node = malloc(sizeof(t_ast));
+    ls_node->type = AST_CMD;
+    char **argv2 = malloc(sizeof(char*) * 3);
+    argv2[0] = "/bin/ls";
+    argv2[1] = "-l";
+    argv2[2] = NULL;
+    ls_node->u_data.cmd.argv = argv2;
+
+    // Pipe: echo | ls -l
+    t_ast *pipe_node = malloc(sizeof(t_ast));
+    pipe_node->type = AST_PIPE;
+    pipe_node->u_data.op.left = echo_node;
+    pipe_node->u_data.op.right = ls_node;
+
+    return pipe_node;
+}
+
+void free_ast(t_ast *node)
+{
+    if (!node)
+        return;
+    if (node->type == AST_CMD)
+        free(node->u_data.cmd.argv); // argv is mallocd
+    else if (node->type == AST_PIPE || node->type == AST_AND_IF || node->type == AST_OR_IF)
+    {
+        free_ast(node->u_data.op.left);
+        free_ast(node->u_data.op.right);
+    }
+    else if (node->type == AST_SUBSH)
+        free_ast(node->u_data.subsh.child);
+    free(node);
+}
+
+// Main TEST-debug
+int main(void)
+{
+    t_ast *root = build_test_ast();
+    printf("Executing AST...\n");
+    int status = exec_ast(root);
+    free_ast(root);
+    printf("Done, status=%d\n", WEXITSTATUS(status));
+    return 0;

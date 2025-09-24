0089392c6905f21bff45ccb8bcd8fcf34042788a
Gabriel Dan Omandam <gomandam@c3r1s1.42madrid.com>
2025-09-24 02:40:10 +0200

    [add] cd

--- source/builtins/cd_rebuild.c
+++ source/builtins/cd_rebuild.c
@@ -6,14 +6,12 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 02:02:59 by gomandam          #+#    #+#             */
-/*   Updated: 2025/09/11 02:08:36 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/09/24 02:39:22 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-
-// ? reference: https://github.com/g01100001/unattributed-source/blob/main/minishell-gonzlo-correction/src/built-ins/cd.c 
+#include "../../libft/libft.h"
 
 void	cd_error(char **argv[2])
 {
@@ -28,25 +26,41 @@ void	cd_error(char **argv[2])
 int	ft_cd(t_shell *shell, char *argv[])
 {
 	char	*cwd;
-	
+
+	cwd = getcwd();	
 	if (!argv[1])
 		return(printf("minishell: cd: error\n"), 0);
 	if (chdir(argv[1]) != 0) 	// chdir function returns 0 on success and -1 on failure. Anything other than 0 is an error occurence.
 		return (0);
-	if (shell->prompt)		// ? 	TODO: retrospect. 
+	if (shell->prompt) 
 		free(shell->prompt);
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 		return (0);
 	
 }
-https://github.com/DanielCasti11o/Mini_Shell/blob/main/src/built_in.c
 
 	// 1. Get command arguments
 	// 2. Retrieve HOME from environment
-	// 3. Build matrix with HOME
-	// 4. Append current working directory to matrix
+	// 3. Check for arguments if not 'HOME'. If home doesnt exist then error
+	// 4. ft_strncmp to find '-' and implement going back to previous directory
 	// 5. Call error handler for cd logic and chdir
-	// 6. If chdir successful, update OLDPWD
-	// 7. Update PWD to new current directory
+	// 5. Use get_env_value() to set OLDPWD
+	// 6. If chdir successful, update OLDPWD, if doesn't exist then print error (also check for exit status)
+	// 7. Update PWD to new current directory, and print
 	// 8. Free and return
+	//
+
+Getcwd to find path when entering CD.
+
+Know which directory are we going to
+
+Check for arguments if not 'HOME'
+Home doesnt exist then error
+
+Strncmp to find '-'
+
+Use get_env_value() to set OLDPWD
+if doesnt exist then print error
+
+Print

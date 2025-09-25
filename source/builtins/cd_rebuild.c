cdcad8cdd8bae998183e9fff61b81f296ce0c59d
root <root@Gthulhu.localdomain>
2025-09-26 00:25:39 +0200

    [add] ft_cd build || norminette errors

--- source/builtins/cd_rebuild.c
+++ source/builtins/cd_rebuild.c
@@ -6,91 +6,137 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 02:02:59 by gomandam          #+#    #+#             */
-/*   Updated: 2025/09/25 03:06:21 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/09/26 00:19:39 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../libft/libft.h"
 
-void	cd_error(char **argv[2])
-{
-	if ()
-	//No argument (cd): goes to $HOME or errors if not set.
-	//Single argument (cd dirname): checks existence, directory type, and calls chdir.
-	//Error messages for missing HOME, not a directory, file not found.
-}
-
-// separate command logic from error handling, updates environment variables
-
-int	ft_cd(t_shell *shell, char *argv[])
+/* Get current working directory with PWD env_get_value()
+   Returns malloc'd string. NULL when fail. */
+static char	*get_current_pwd(t_shell *shell)
 {
 	char	*cwd;
+	char	*pwd_value;
 
-	cwd = getcwd();	
-	if (!argv[1])
-		return(printf("minishell: cd: error\n"), 0);
-	if (chdir(argv[1]) != 0) 	// chdir function returns 0 on success and -1 on failure. Anything other than 0 is an error occurence.
-		return (0);
-	if (shell->prompt) 
-		free(shell->prompt);
 	cwd = getcwd(NULL, 0);
-	if (!cwd)
-		return (0);
-	
+	if (cwd)
+		return (cwd);
+	if (errno == ENOMEM)
+	{
+		perror_malloc(shell);
+		return (NULL);
+	}
+	pwd_value = env_get_value(&shell->env_list, "PWD");
+	if (pwd_value)
+		return (ft_strdup(pwd_value));
+	return (NULL);
 }
 
-	// 1. Get command arguments
-	// 2. Retrieve HOME from environment
-	// 3. Check for arguments if not 'HOME'. If home doesnt exist then error
-	// 4. ft_strncmp to find '-' and implement going back to previous directory
-	// 5. Call error handler for cd logic and chdir
-	// 5. Use get_env_value() to set OLDPWD
-	// 6. If chdir successful, update OLDPWD, if doesn't exist then print error (also check for exit status)
-	// 7. Update PWD to new current directory, and print
-	// 8. Free and return
-	//
-
-Getcwd to find path when entering CD.
-
-Know which directory are we going to
-
-Check for arguments if not 'HOME'
-Home doesnt exist then error
-
-Strncmp to find '-'
-
-Use get_env_value() to set OLDPWD
-if doesnt exist then print error
-
-Print
-//
-
-int	cd_command(char ** cmd_info, t_data *data)
+/* OLDPWD environment variable with given directory.
+   Uses env_upsert() pattern from init.c ensure_pwd_env() */
+static int	update_oldpwd_env(t_shell *shell, const char *old_dir)
 {
-	char	*target_dir;
-	char	*current_pwd;
+	char	*oldpwd_line;
 
-	current_pwd = getcwd(NULL,0);
-	if (!current_pwd)
-		current_pwd = get_env_value(data->env, "PWD");
-	target_dir = get_cd_target(data, cmd_info);
-	if (!target_dir)
+	if (!old_dir)
+		return (0);
+	oldpwd_line = ft_strjoin("OLDPWD=", old_dir);
+	if (!oldpwd_line)
+		return (perror_malloc(shell));
+	if (env_upsert(&shell->env_list, oldpwd_line, oldpwd_line + 7) == 1)
 	{
-		cleanup_cd_pwd(data, current_pwd);
+		free(oldpwd_line);
+		return (perror_malloc(shell));
+	}
+	free(oldpwd_line);
+	return (0);
+}
+
+/* Updates PWD environment variable with current directory.
+   ensure_pwd_env() from init.c exactly. */
+static int	ensure_pwd_env(t_shell *shell)
+{
+	char	*cwd;
+	char	*pwd_line;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		if (errno == ENOMEM)
+			return (perror_malloc(shell));
+		perror("minishell: cd: getcwd");
 		return (1);
 	}
+	pwd_line = ft_strjoin("PWD=", cwd);
+	free(cwd);
+	if (!pwd_line)
+		return (perror_malloc(shell));
+	if (env_upsert(&shell->env_list, pwd_line, pwd_line + 4) == 1)
+	{
+		free(pwd_line);
+		return (perror_malloc(shell));
+	}
+	free(pwd_line);
+	return (0);
+}
+
+/*  Resolve cd target: no args->HOME, "-"->OLDPWD, else->path.
+    returns malloc target or NULL on error. Prints to stdout for "cd -". */
+static char	*resolve_cd_target(t_shell *shell, char **argv)
+{
+	char	*target;
+
+	if (!argv[1])
+	{
+		target = env_get_value(&shell->env_list, "HOME");
+		if (!target)
+			return(ft_putstr_fd("minishell: cd: HOME not set\n", 2), set_last_status(shell, 1), NULL);
+		return (ft_strdup(target));
+	}
+	if (argv[2])
+		return(ft_putstr_fd("minishell: cd: too many arguments\n", 2), set_last_status(shell, 1), NULL);
+	if (!ft_strcmp(argv[1], "-"))
+	{
+		target = env_get_value(&shell->env_list, "OLDPWD");
+		if (!target)
+		{
+			ft_putstr_fd("minishell: cd: OLDPWD not set\n", 2);
+			return(set_last_status(shell, 1), NULL);
+		}
+		ft_putendl_fd(target, 1);
+		return (ft_strdup(target));
+	}
+	return (ft_strdup(argv[1]));
+}
+
+/* Main ft_cd: change directory and update PWD/OLDPWD.
+    env functions: env_get_value, env_upsert */
+int	ft_cd(t_shell *shell, char **argv)
+{
+	char	*old_pwd;
+	char	*target_dir;
+
+	old_pwd = get_current_pwd(shell);
+	target_dir = resolve_cd_target(shell, argv);
+	if (!target_dir)
+		return(free(old_pwd), 1);
 	if (chdir(target_dir) == -1)
 	{
-		perror("cd");
-		cleanup_cd_pwd(data, current_pwd);
+		perror("minishell: cd");
+		free(old_pwd);
+		free(target_dir);
+		set_last_status(shell, 1);
 		return (1);
 	}
-	if (current_pwd)
-		update_oldpwd(data, current_pwd);
-	else
-		update_oldpwd(data, "");
-	update_pwd(data);
-	cleanup_cd_pwd(data, current_pwd);
-	return (0);
+	free(target_dir);
+	if (old_pwd)
+	{
+		update_oldpwd_env(shell, old_pwd);
+		free(old_pwd);
+	}
+	if (ensure_pwd_env(shell) != 0)
+		return(set_last_status(shell, 1), 1);
+	return(set_last_status(shell, 0), 0);
 }

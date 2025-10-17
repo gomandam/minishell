97a7db9bd86b6696fd605ca48582d8877dd07ab7
gomandam <gomandam@student.42madrid.com>
2025-10-18 00:38:49 +0200

    [add] documentation: builtin_exec.c pipe_exec.c redirections.c

--- source/execute/builtin_exec.c
+++ source/execute/builtin_exec.c
@@ -3,15 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exec.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: migugar2 <migugar2@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 00:24:27 by gomandam          #+#    #+#             */
-/*   Updated: 2025/10/02 22:33:45 by gomandam         ###   ########.fr       */
+/*   Created: 2025/10/18 00:03:54 by gomandam          #+#    #+#             */
+/*   Updated: 2025/10/18 00:33:16 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ============================================================================
+ * FILE: builtin_exec.c
+ * ============================================================================
+ * PURPOSE: Builtin command dispatcher - identifies and executes shell builtins
+ *          with proper redirection handling.
+ *
+ * CONTROL FLOW:
+ *   get_builtin (string -> enum mapping)
+ *   run_builtin
+ *     ├─> resolve_redirs (redirections.c)
+ *     ├─> ft_exit/pwd/env/unset/echo/export/cd (builtins/ *.c)
+ *     └─> close_no_std (cleanup fds)
+ *
+ * BUILTINS:
+ *   1. echo   - Print arguments [builtins/echo.c]
+ *   2. cd     - Change directory [builtins/cd.c]
+ *   3. pwd    - Print working directory [builtins/pwd.c]
+ *   4. export - Set environment variables [builtins/export.c]
+ *   5. unset  - Unset environment variables [builtins/unset.c]
+ *   6. env    - Print environment [builtins/env.c]
+ *   7. exit   - Exit shell [builtins/exit.c]
+ *
+ * EXTERNAL USAGE: run_cmd [cmd_exec.c]
+ * NOTE: Builtins execute in parent process, can modify shell state
+ * =========================================================================== */
 #include "minishell.h"
 
+/* FUNCTION: get_builtin
+ * PURPOSE: Map command string to builtin enum type.
+ * PARAMS: @cmd - command name string
+ * RETURN: t_builtin enum (B_CMD if not builtin)
+ * NOTE: Case-sensitive exact match required  */
 t_builtin	get_builtin(char *cmd)
 {
 	if (!cmd)
@@ -33,6 +63,12 @@ t_builtin	get_builtin(char *cmd)
 	return (B_CMD);
 }
 
+/* FUNCTION: run_builtin
+ * PURPOSE: Execute builtin command with redirections in parent process.
+ * PARAMS: @shell - state, @ast - command AST, @pid - set to 0, @type - builtin
+ * RETURN: Builtin execution status
+ * BEHAVIOR: Resolve redirections to fds[2], dispatch to builtin, cleanup fds
+ * NOTE: Sets *pid=0 to signal no child process; fds[0]=in, fds[1]=out  */
 int	run_builtin(t_shell *shell, t_ast **ast, pid_t *pid, t_builtin type)
 {
 	int	status;
